e2a41840ba34c8147aee40c761e81475230386c9
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2023-10-20 23:25:41 +0300

    init commit, add first routes

---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+function About() {
+    return <h1>It is the about page, friend</h1>
+}
+
+export default About;
---
+++ app/profile/page.js
@@ -0,0 +1,5 @@
+function Profile () {
+    return <h1>It is your profile, friend!</h1>
+}
+
+export default Profile;

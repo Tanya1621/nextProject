944ac72cc4ac460d06387d5cc0921870db5dfed1
Tanya1621 <70881555+Tanya1621@users.noreply.github.com>
2024-01-14 00:01:11 +0200

    add an image

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Image from 'next/image';
+
 function Home () {
-    return <h1>Hello, friend!</h1>
+    return <div><h1>Hello, friend!</h1>
+    <Image
+        src="/images/welcome-text.jpeg"
+        height={500}
+        width={500}
+        alt="welcome"
+    /></div>
 }
 
 export default Home;
